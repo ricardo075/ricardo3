decf96023af5e8e4e274e5ccc3b264725543c7a6
ricardo075 <155269880+ricardo075@users.noreply.github.com>
2024-01-08 21:12:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.set_page_config(page_title='TEMAS DA MATERIA DE MATEMATICA')
+
+st.title('CALCULADORA DE EQUAÇAO QUADRATICA')
+st.write('DELTA X1 E X2[CLIQUE AQUI]'
+         '(https://ricardo34-axdhnxr9cm4zjuxqpanre5.streamlit.app/)')
+
+st.title('CALCULADORA DE TEORIA DE CONJUNTOS.')
+st.write('POR EXEMPLO: NUMERO TOTAL DOS PROFESSORES DA ESCOLA, (A) ENSINAM PORTUGUES,B ENSINAM INGLES,C NAO ENSIAM '
+         'NENHUMA.'
+         '..'
+         '[CLIQUE AQUI]'
+         '(https://ricardo7-9t4bqhysyd99ntsqyfqueq.streamlit.app/)')
+
+st.write('O SITE AINDA ESTA EM DESENVOLVIMENTO, AGUARDEM POIS TEREMOS MAIS OPÇOES  DE CALCULOS'
+         ' E AULAS COMPLETAS, EXPLICANDO COMO SAO FEITOS OS CALCULOS PASSO A PASSO')
+st.write('MAIS INFORMAÇOES E DUVIDAS CONTACTE 841038887/868787572')
